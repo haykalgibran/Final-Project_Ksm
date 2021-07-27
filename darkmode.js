b6fcf5179d4d4e93119c9355260646d73e423086
haykalgibran <hykalgibran17@gmail.com>
2021-07-27 22:02:42 +0700

    Final-Project_Ksm

---
+++ darkmode.js
@@ -0,0 +1,37 @@
+function setDarkMode(isDark) {
+  var darkBtn = document.getElementById("darkBtn");
+  var lightBtn = document.getElementById("lightBtn");
+
+  if (isDark) {
+    lightBtn.style.display = "block";
+    darkBtn.style.display = "none";
+  } else {
+    lightBtn.style.display = "none";
+    darkBtn.style.display = "block";
+  }
+
+  document.body.classList.toggle("darkmode");
+}
+//check localstorage
+if (localStorage.getItem("preferredTheme") == "dark") {
+  setDarkMode(true);
+}
+
+function setDarkMode(isDark) {
+  var darkBtn = document.getElementById("darkBtn");
+  var lightBtn = document.getElementById("lightBtn");
+
+  if (isDark) {
+    lightBtn.style.display = "block";
+    darkBtn.style.display = "none";
+    //tambahan localstorage
+    localStorage.setItem("preferredTheme", "dark");
+  } else {
+    lightBtn.style.display = "none";
+    darkBtn.style.display = "block";
+    //tambahan localstorage
+    localStorage.removeItem("preferredTheme");
+  }
+
+  document.body.classList.toggle("darkmode");
+}
